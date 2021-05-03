f2ef8aedf00f3e82d41c28c54d5ddfad741251b8
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-05-03 12:27:15 -0500

    update

--- src/day01_intro_to_programming/HelloWorld.java
+++ src/day01_intro_to_programming/HelloWorld.java
@@ -2,7 +2,7 @@ package day01_intro_to_programming;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World 1!");
     }
 }
 
