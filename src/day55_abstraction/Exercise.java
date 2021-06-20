2e134120aefbce6e025ee1da06b5facd05f7ac07
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-20 10:52:43 -0500

    abtraction exercise

---
+++ src/day55_abstraction/Exercise.java
@@ -0,0 +1,13 @@
+package day55_abstraction;
+
+public abstract class Exercise {
+
+    public void start(int minutes){
+        System.out.println("Warming up and starting the exercise ");
+    }
+    public abstract void perform() ;
+
+
+    public abstract  int calories(int minutes);
+
+    }
