8e7aee2337436783a0a319424060d162925830f7
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-23 12:34:38 -0500

    finish practice

---
+++ src/practice_my_self/AAA.java
@@ -0,0 +1,12 @@
+package practice_my_self;
+
+public class AAA {
+    public  static int num1 = 100;
+    private  static int num2 = 200;
+    protected static int num3 = 300;
+     static int num4 = 400;
+
+
+
+    }
+
